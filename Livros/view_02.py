bfda14225ab4b0b4859e95ad8235f99193cbcb9b
Mikael De Souza Dias <mikaelsd1919@icloud.com>
2024-04-30 17:57:00 -0300

    update_30/04/2023/2

--- Livros/view_02.py
+++ Livros/view_02.py
@@ -9,8 +9,19 @@ def connect():
 # função para inserir um novo livro
 def insert_ebook(titulo, autor, editora, ano_publicacao, isbn):
     con = connect()
-    con.execute("INSERT INTO livro(titulo, autor, editora, ano_publicacao, isbn)\
-                 VALUES(?,?,?,?,?)",(titulo, autor, editora, ano_publicacao, isbn))
+    try:
+        con.execute("INSERT INTO livro(titulo, autor, editora, ano_publicacao, isbn) VALUES(?,?,?,?,?)", (titulo, autor, editora, ano_publicacao, isbn))
+        con.commit()
+    except sqlite3.OperationalError as e:
+        # Se ocorrer um erro de bloqueio do banco de dados, tentar novamente após 1 segundo
+        if "database is locked" in str(e):
+            print("Erro: O banco de dados está bloqueado. Tentando novamente após 1 segundo.")
+            time.sleep(1)
+            insert_ebook(titulo, autor, editora, ano_publicacao, isbn)
+        else:
+            print("Erro ao inserir o livro:", e)
+    finally:
+        con.close()
     
 
 # Funcao para inserir usuários
@@ -22,7 +33,7 @@ def insert_user(nome, sobrenome, endereco, email, telefone):
     con.commit()
     con.close()
 # exemplo de funcoes
-insert_ebook('DoM quixote','miquel', 'editora_10,', 1605, '12346')    
+
 
 # insert_user('Mikael', 'Dias', 'Angola/Portugal', 'mikaelsd1010@icloud.com', '84555555222')
 
@@ -66,19 +77,35 @@ def insert_loan(id_livro, id_usuario, data_emprestimo, data_devolucao):
 #funcao para exibir todos os livros emprestados no momento
 def get_books_on_loan():    
     con = connect()
-    result = con.execute('SELECT livro.titulo, usuario.nome, usuario.sobrenome, emprestimo.data_emprestimo, emprestimo.data_devolucao\
-                         FROM livros\
+    result = con.execute('SELECT livro.titulo, usuario.nome, usuario.sobrenome, emprestimo.id, emprestimo.data_emprestimo, emprestimo.data_devolucao\
+                         FROM livro\
                          INNER JOIN emprestimo ON livro.id = emprestimo.id_livro\
                          INNER JOIN  usuario ON usuario.id = emprestimo.id_usuario\
                          WHERE emprestimo.data_devolucao IS NULL').fetchall()
     con.close()
     return result
 
-insert_loan(1, 1, '2024/04/30', None)
 
-
-
-
-
+# funçao para tualizar a data de devolução de emprestimo
+def update_loan_return_date(id_emprestimo, data_devolucao):
+    con = connect()
+    try:
+        con.execute('UPDATE emprestimo SET data_devolucao = ? WHERE id = ?', (data_devolucao, id_emprestimo))
+        con.commit()
+    except sqlite3.OperationalError as e:
+        # Se ocorrer um erro de bloqueio do banco de dados, tentar novamente após 1 segundo
+        if "database is locked" in str(e):
+            print("Erro: O banco de dados está bloqueado. Tentando novamente após 1 segundo.")
+            time.sleep(1)
+            update_loan_return_date(id_emprestimo, data_devolucao)
+        else:
+            print("Erro ao atualizar a data de devolução do empréstimo:", e)
+    finally:
+        con.close()
+insert_loan(1, 1, '2024/03/30', None)
+livros_emprestados = get_books_on_loan()
+'''insert_ebook('Dom Quixote', 'Miguel de Cervantes', 'Editora X', 1605, '9781234567890')'''
+update_loan_return_date(6, '2024-04-30')
+'''print(livros_emprestados)'''
 
 exibir_livros()
